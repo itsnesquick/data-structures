2051665a8d961ed8f0d870ab326eac7e7bdcc35a
Nesquick <nesquick@Nesquicks-MBP.fritz.box>
2020-04-11 19:34:20 +0200

    Adding methods inside the LinkedLIst class

--- linked_list.py
+++ linked_list.py
@@ -24,3 +24,39 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+
+    def push(self):
+        """
+        Description: Inserts given element at the end of the linked list
+        """
+        pass
+
+    def delete(self):
+        """
+        Description: Deletes given element from the linked list
+        """
+        pass
+
+    def unshift(self):
+        """
+        Description: Deletes the first element of the linked list
+        """
+        pass
+
+    def shift(self):
+        """
+        Description: Inserts given element at the head of the linked list
+        """
+        pass
+
+    def search(self):
+        """
+        Description: Returns the given element from linked list
+        """
+        pass
+
+    def isEmpty(self):
+        """
+        Description: Returns if the linked list is empty
+        """
+        pass
